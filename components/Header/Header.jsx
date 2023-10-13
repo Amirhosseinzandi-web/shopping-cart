577ca12e1eddbbe90c73bba3978bddc02482a8a9
Amirhosseinzandi-web <blackviper933@gmail.com>
2023-10-13 19:12:45 +0330

    add sticky header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,28 @@
 "use client"
 
 
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 
 
 const Header = () => {
     const { products } = useSelector(state => state.app);
+    const [state, setState] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            let windowScroll = document.documentElement.scrollTop;
+            let header = document.querySelector("header");
+            let headerHeight = header.offsetHeight;
+
+           
+                header.classList.toggle("sticky-header" , windowScroll > Number(headerHeight) + 50);
+            
+        });
+    }, [state]);
+
+
     return (
         <header>
             <section className="container mx-auto px-16 py-7">
